2e80863e91e69922d31b0b38fae3cd3fc217c444
sparksys98 <hanineiroukh@gmail.com>
2020-03-16 15:43:29 +0200

    >.<

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
-
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
 import AuthorDetail from "./AuthorDetail";
-
+import Loading from "./Loading";
+import axios from "axios";
 class App extends Component {
   state = {
-    currentAuthor: null
+    authors: [],
+    currentAuthor: null,
+    loading: true
   };
-
-  selectAuthor = author => this.setState({ currentAuthor: author });
-
+  getAuthors = async () => {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({ authors, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  componentDidMount() {
+    this.getAuthors();
+  }
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    try {
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+      );
+      const currentAuthor = response.data;
+      this.setState({ currentAuthor, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   getContentView = () => {
+    console.log(this.state.authors);
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+      return (
+        <AuthorList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
     }
   };
 
@@ -31,7 +60,11 @@ class App extends Component {
           <div className="col-2">
             <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">{this.getContentView()}</div>
+          {this.state.loading ? (
+            <Loading />
+          ) : (
+            <div className="content col-10">{this.getContentView()}</div>
+          )}
         </div>
       </div>
     );
---
+++ src/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Loading = () => (
+  <div>
+    <h2>Not the best loading you may ever see...</h2>
+  </div>
+);
+
+export default Loading;
